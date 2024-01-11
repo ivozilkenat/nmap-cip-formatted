8c46b12841e201c7a2ced97baf957bdb3b49abfb
hoischreck <hoischreck@gmx.de>
2024-01-11 15:55:05 +0100

    skip rescanning

--- nmapScenarioScan.py
+++ nmapScenarioScan.py
@@ -8,8 +8,20 @@ import netifaces as ni
 import ipaddress
 import xml.etree.ElementTree as ET
 import sys
+import os
 
 import nmap2cip
+
+def run_if_file_not_existing(path):
+    def __inner(func):
+        def __innerinner(*args, **kwargs):
+            if os.path.isfile(path):
+                print("> FILE FOUND: {path}")
+                return 
+            return func(*args, **kwargs)
+        return __innerinner
+    return __inner
+            
 
 def check_rcode(return_code, message):
     if return_code != 0:
@@ -117,12 +129,19 @@ if __name__ == "__main__":
     print_format = "[>>> {} <<<]"
     
     print(print_format.format("SCANNING IPv4 - local"))
-    scan_network(ip, local_prefix_len, NMAP_OUT_FILE_BASE.format("local-ipv4")) # TEST
+    nmap_out_file_name_ipv4_local = NMAP_OUT_FILE_BASE.format("local-ipv4")
+    run_if_file_not_existing(nmap_out_file_name_ipv4_local)(scan_network)(
+        ip, local_prefix_len, nmap_out_file_name_ipv4_local
+    ) # TEST
     # scan ipv 6    
     print(print_format.format("DISCOVERING IPv6 - local"))
-    scan6_local(args.interface, SCAN6_OUT_FILE_BASE)
+    run_if_file_not_existing(SCAN6_OUT_FILE_BASE)(scan6_local)(
+        args.interface, SCAN6_OUT_FILE_BASE   
+    )
     print(print_format.format("SCANNING IPv6 - local"))
-    scan_network_file(SCAN6_OUT_FILE_BASE, nmap_out_file_name_ipv6)
+    run_if_file_not_existing(nmap_out_file_name_ipv6)(scan_network_file)(
+        SCAN6_OUT_FILE_BASE, nmap_out_file_name_ipv6
+    )
     
     # eval data
     scan_evaluation(
@@ -139,7 +158,9 @@ if __name__ == "__main__":
             continue
         nmap_out_file_name = NMAP_OUT_FILE_BASE.format(ip)
         print(print_format.format("SCANNING IPv4 - {r_ip}"))
-        scan_network(r_ip, ROUTER_PREFIX_GUESS, nmap_out_file_name)
+        run_if_file_not_existing(nmap_out_file_name)(scan_network)(
+            r_ip, ROUTER_PREFIX_GUESS, nmap_out_file_name
+        )
         
         # eval data here
         scan_evaluation(DATA_EVAL_FILE_BASE.format(r_ip), nmap_out_file_name)
